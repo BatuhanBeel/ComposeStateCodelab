864db4739b2f7f922b48593106d3229a2b2849b4
Rexuses <batuhan-498@hotmail.com>
2024-04-04 15:26:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/composestatecodelab/WellnessViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.composestatecodelab
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+class WellnessViewModel(): ViewModel() {
+
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val tasks: List<WellnessTask>
+        get() = _tasks
+
+    fun removeTask(task: WellnessTask){
+        _tasks.remove(task)
+    }
+
+    fun changeTaskChecked(item: WellnessTask, checked: Boolean) =
+        tasks.find { it.id == item.id }?.let { task ->
+        task.checked = checked
+    }
+}
+
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
